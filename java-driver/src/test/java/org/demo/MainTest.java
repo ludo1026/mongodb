50f515de30c763afc2dbbfcdf67e133986535fe7
ludo1026 <ludo1026@free.fr>
2015-03-02 18:20:09 +0100

    mongodb

---
+++ java-driver/src/test/java/org/demo/MainTest.java
@@ -0,0 +1,18 @@
+package org.demo;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void testMain() {
+        // Given
+        String[] args = null;
+
+        // When
+        Main.main(args);
+
+        // Then
+    }
+
+}
